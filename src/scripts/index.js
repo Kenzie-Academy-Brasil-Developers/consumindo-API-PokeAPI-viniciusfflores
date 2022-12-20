7615b85df8dd463f4351f010be7aa1a61dac7eef
Viniciusfflores <vff.tattoo@gmail.com>
2022-12-20 14:30:23 -0300

    pokeAPI 1 sem search funcional

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,21 @@
-/* Monte sua lógica aqui */
+
+async function renderPokemons() {
+    const ulTag = document.querySelector('.poke-ul')
+
+ 
+    const pokeList = await pokeAPI()
+
+   
+    pokeList.results.forEach(pokemon => {
+       
+        const numPokedex = pokemon.url.slice(34, -1)
+
+        ulTag.insertAdjacentHTML('beforeend', `
+            <li class="poke-card">
+                <img class="poke-icone" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numPokedex}.png" alt=${pokemon.name}>
+                <strong class="poke-name">${pokemon.name}</strong>
+            </li>
+        `)
+    })
+}
+renderPokemons()
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,21 @@
-/* Monte a lógica das requisições aqui */
+async function pokeAPI() {
+  
+    const loading = document.querySelector('#loading')
+
+ 
+    const pokemonsDaAPI = await fetch('https://pokeapi.co/api/v2/pokemon')
+      .then(
+        response => response.json()
+      )
+      .catch(
+ 
+        error => console.log(error)
+      )
+
+    loading.classList.add('hidden')
+
+  
+    return pokemonsDaAPI
+}
+console.log(pokeAPI())
+
